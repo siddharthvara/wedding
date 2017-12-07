d5ab972fe95adb580dfe87cc02557f1c596ad332
siddharthvara <siddharth.vara@gmail.com>
2017-12-07 23:23:08 +0530

    add header image

--- index.php
+++ index.php
@@ -85,7 +85,7 @@
 								<li><a href="#">jQuery</a></li>
 							</ul>-->
 						</li>
-						<li><a href="contact.html">Contact</a></li>
+						<!-- <li><a href="contact.html">Contact</a></li> -->
 					</ul>
 				</div>
 			</div>
@@ -93,14 +93,14 @@
 		</div>
 	</nav>
 
-	<header id="fh5co-header" class="fh5co-cover" role="banner" style="background-image:url(images/img_bg_2.jpg);" data-stellar-background-ratio="0.5">
+	<header id="fh5co-header" class="fh5co-cover" role="banner" style="background-image:url(images/sid.jpg);" data-stellar-background-ratio="0.5">
 		<div class="overlay"></div>
 		<div class="container">
 			<div class="row">
 				<div class="col-md-8 col-md-offset-2 text-center">
 					<div class="display-t">
 						<div class="display-tc animate-box" data-animate-effect="fadeIn">
-							<h1>Joefrey &amp; Sheila</h1>
+							<h1>Siddharth &amp; Jigisha</h1>
 							<h2>We Are Getting Married</h2>
 							<div class="simply-countdown simply-countdown-one"></div>
 							<p><a href="#" class="btn btn-default btn-sm">Save the date</a></p>
